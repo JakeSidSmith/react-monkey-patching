94e49743040e3dcaec770e4da752c647a8741eb0
Jake 'Sid' Smith <jake@dabapps.com>
2017-11-04 18:03:55 +0000

    Some example components

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 
+const ArrayComponent = () => (
+  [
+    'Array',
+    'Items'
+  ]
+);
+
+const StringComponent = () => (
+  'String'
+);
+
 export default class App extends Component {
   render () {
     return (
-      <p>
-        Hello, World!
-      </p>
+      <div>
+        <p>
+          Hello, World!
+        </p>
+        <p>
+          <ArrayComponent />
+        </p>
+        <p>
+          <StringComponent />
+        </p>
+        <p>
+          <input type="submit" value="Input Submit" />
+        </p>
+        <p>
+          <input type="button" value="Input Button" />
+        </p>
+        <p>
+          <input type="text" placeholder="Input Text Placeholder" />
+        </p>
+      </div>
     );
   }
 }
