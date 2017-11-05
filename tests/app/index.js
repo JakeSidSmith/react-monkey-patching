6cc07e73ef61fb1bcfa8decea400695807f600b3
Jake 'Sid' Smith <jake@dabapps.com>
2017-11-05 15:03:23 +0000

    Test llamify and scramble

--- tests/app/index.js
+++ tests/app/index.js
@@ -5,7 +5,25 @@ import App from '../../src/js/app/';
 
 describe('App', () => {
 
+  const originalCreateElement = React.createElement;
+
+  beforeEach(() => {
+    React.createElement = originalCreateElement;
+  });
+
   it('should match snapshot', () => {
+    expect(renderer.create(<App />)).toMatchSnapshot();
+  });
+
+  it('should match snapshot when llamified', () => {
+    require('../../src/js/monkey-patches/llamify');
+
+    expect(renderer.create(<App />)).toMatchSnapshot();
+  });
+
+  it('should match snapshot when scrambled', () => {
+    require('../../src/js/monkey-patches/scramble');
+
     expect(renderer.create(<App />)).toMatchSnapshot();
   });
 
