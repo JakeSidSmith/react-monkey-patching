732f28c42711879c539772062e3ea4db9f135093
Jake 'Sid' Smith <jake@dabapps.com>
2017-11-06 22:14:23 +0000

    Fix case sensitive import

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import './livereload';
 // import './monkey-patches/scramble';
 // import './monkey-patches/llamify';
 
-import React from 'React';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './app/';
