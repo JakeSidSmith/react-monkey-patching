0122f2cadeebdfba46b88bad9d837c7bbf490833
Jake 'Sid' Smith <jake@dabapps.com>
2017-11-06 21:18:16 +0000

    Fix linting

--- src/js/app/pages/monkey-patching.js
+++ src/js/app/pages/monkey-patching.js
@@ -8,7 +8,8 @@ export default class MonkeyPatching extends React.PureComponent {
           Wikipedia says:
         </h2>
         <p>
-          A monkey patch is a way for a program to extend or modify supporting system software locally (affecting only the running instance of the program).
+          A monkey patch is a way for a program to extend or modify supporting system software locally
+          (affecting only the running instance of the program).
         </p>
         <p>
           <a title="Wikipedia - Monkey patch" href="https://en.wikipedia.org/wiki/Monkey_patch" target="_blank">
@@ -19,7 +20,8 @@ export default class MonkeyPatching extends React.PureComponent {
           So why's it called that?
         </h2>
         <p>
-          The term monkey patch seems to have come from an earlier term, guerrilla patch, which referred to changing code sneakily – and possibly incompatibly with other such patches – at runtime.
+          The term monkey patch seems to have come from an earlier term, guerrilla patch, which referred to
+          changing code sneakily – and possibly incompatibly with other such patches – at runtime.
         </p>
         <h2>
           What's it good for?
@@ -37,7 +39,8 @@ export default class MonkeyPatching extends React.PureComponent {
             Apply a patch at runtime to the objects in memory, instead of the source code on disk;
           </li>
           <li>
-            Distribute security or behavioural fixes that live alongside the original source code (an example of this would be distributing the fix as a plugin for the Ruby on Rails platform).
+            Distribute security or behavioural fixes that live alongside the original source code (an example of
+            this would be distributing the fix as a plugin for the Ruby on Rails platform).
           </li>
         </ul>
       </div>
