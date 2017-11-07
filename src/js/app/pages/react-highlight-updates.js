3f7ed3297c688202b5d90f95d061fc1760740172
Jake 'Sid' Smith <jake@dabapps.com>
2017-11-07 15:41:43 +0000

    Highlight updates info & link

--- src/js/app/pages/react-highlight-updates.js
+++ src/js/app/pages/react-highlight-updates.js
@@ -7,6 +7,33 @@ export default class ReactHighlightUpdates extends PatchableComponent {
     return (
       <div>
         <ApplyButton patch={'highlightUpdates'} />
+        <h2>
+          What is React Highlight Updates?
+        </h2>
+        <p>
+          I designed this small library a short time ago to highlight when components have updated.
+          This comes in handy when debugging certain issues.
+        </p>
+        <p>
+          You can check it out on npm here:
+          <a
+            title="React Highlight Updates on npm"
+            href="https://www.npmjs.com/package/react-highlight-updates"
+            target="_blank"
+          >
+            React Highlight Updates on npm - npmjs.com/package/react-highlight-updates
+          </a>
+        </p>
+        <h2>
+          What does it do?
+        </h2>
+        <p>
+          This package monkey patches components' `prototype.componentDidUpdate` method & applies an animation
+          to the elements background color.
+        </p>
+        <p>
+          Press the "Apply monkey patch" button above to give it a go now.
+        </p>
       </div>
     );
   }
