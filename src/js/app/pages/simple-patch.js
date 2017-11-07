83c648d1302c54d4a1c9099f7a5978b4f28a3d6f
Jake 'Sid' Smith <jake@dabapps.com>
2017-11-07 17:23:09 +0000

    Simple patch function

--- src/js/app/pages/simple-patch.js
+++ src/js/app/pages/simple-patch.js
@@ -5,7 +5,35 @@ export default class SimplePatch extends PatchableComponent {
   render () {
     return (
       <div>
-        Simple patch
+        <h2>
+          A basic example
+        </h2>
+        <p>
+          Below is an incredibly cut down version of the kind of monkey patch that the following few examples use.
+        </p>
+        <pre>
+{`// Store the original function
+const originalCreateElement = React.createElement;
+
+// Define a new function
+React.createElement = function () {
+  // Get our arguments as an array
+  const args = Array.prototype.slice.call(arguments);
+
+  // Loop over the children (anything after the first 2 arguments)
+  // And transform them if they are strings
+  args.forEach((arg, index) => {
+    if (index >= 2 && typeof arg === 'string') {
+      args[index] = arg.replace(/\b\w+\b/gi, 'llama');
+    }
+  });
+
+  // Apply args to the original function
+  return originalCreateElement.apply(React, args);
+};
+
+`}
+        </pre>
       </div>
     );
   }
