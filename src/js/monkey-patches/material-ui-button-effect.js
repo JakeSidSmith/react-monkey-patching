febbc1ad98a4e8d95a86fea91aeb3c46edea7a09
Jake 'Sid' Smith <jake@dabapps.com>
2017-11-08 17:57:13 +0000

    Style material UI button effects

--- src/js/monkey-patches/material-ui-button-effect.js
+++ src/js/monkey-patches/material-ui-button-effect.js
@@ -1,7 +1,40 @@
 import React from 'react';
 
-const HIGHLIGHT_CLASS = 'react-update-highlight';
-const MATCHES_HIGHLIGHT_CLASS = /\s*\breact-update-highlight\b\s*/gi;
+const STYLE = document.createElement('style');
+STYLE.type = 'text/css';
+
+STYLE.innerHTML = `
+  .material-ui-button-effect {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-top: -50%;
+    margin-left: -50%;
+    width: 100%;
+    height: 0;
+    padding-bottom: 100%;
+    border-radius: 50%;
+    background-color: #FFFFFF;
+    mix-blend-mode: overlay;
+    animation-name: material-ui-button-effect;
+    animation-duration: 1s;
+    animation-iteration-count: 1;
+    animation-timing-function: ease-out;
+    animation-fill-mode: forwards;
+  }
+  @keyframes material-ui-button-effect {
+    from {
+      opacity: 0.5;
+      transform: scale(0, 0);
+    }
+    to {
+      opacity: 0;
+      transform: scale(2, 2);
+    }
+  }
+`;
+
+document.head.appendChild(STYLE);
 
 const materialUIButtonEffect = () => {
   const originalCreateElement = React.createElement;
@@ -23,22 +56,27 @@ const materialUIButtonEffect = () => {
     if (element === 'button') {
       const originalOnClick = props.onClick;
 
+      props.style = Object.assign({}, {position: 'relative', overflow: 'hidden'}, props.style);
+
       props.onClick = function (event) {
         const onClickArgs = Array.prototype.slice.call(arguments);
 
-        const node = event.target;
+        const node = event.currentTarget;
 
         if (!node) {
           return;
         }
 
-        if (MATCHES_HIGHLIGHT_CLASS.test(node.className)) {
-          node.className = node.className.replace(MATCHES_HIGHLIGHT_CLASS, '');
-        }
+        const pew = document.createElement('div');
+        pew.className = 'material-ui-button-effect';
 
-        void node.offsetWidth; // tslint:disable-line:no-unused-expression
-        const needsSpace = node.className || node.className.lastIndexOf(' ') !== node.className.length - 1;
-        node.className = `${node.className}${needsSpace ? ' ' + HIGHLIGHT_CLASS : HIGHLIGHT_CLASS}`;
+        node.appendChild(pew);
+
+        setTimeout(() => {
+          if (pew) {
+            pew.remove();
+          }
+        }, 2000);
 
         if (typeof originalOnClick === 'function') {
           return originalOnClick.apply(this, onClickArgs);
