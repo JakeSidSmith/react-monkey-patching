5a85aeebdc3116fcf9d04e46764a1e6008195e97
Jake 'Sid' Smith <jake@dabapps.com>
2017-11-08 18:00:58 +0000

    More info

--- src/js/app/pages/material-ui.js
+++ src/js/app/pages/material-ui.js
@@ -10,6 +10,13 @@ export default class MaterialUI extends PatchableComponent {
         <h2>
           Get the Google look
         </h2>
+        <p>
+          No need to change any of your components or write fancy styles & js to get that look - just apply a monkey
+          patch and instantly all of your buttons have that sexy Google look.
+        </p>
+        <p>
+          Note: the apply & remove patch buttons have monkey patching disabled in this demo.
+        </p>
         <button>
           Test button!
         </button>
