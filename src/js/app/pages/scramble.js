d7c58bb684e77c4ee093eb0e1c33c2478ddcb109
Jake 'Sid' Smith <jake@dabapps.com>
2017-11-08 14:18:44 +0000

    Info on llamify and scramble

--- src/js/app/pages/scramble.js
+++ src/js/app/pages/scramble.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ApplyButton from '../apply-button';
 import PatchableComponent from '../../monkey-patches/patchable-component';
+import BasicElements from '../basic-elements';
 
 export default class Scramble extends PatchableComponent {
   render () {
     return (
       <div>
         <ApplyButton patch="scramble" />
+        <h2>
+          Euxcse me?
+        </h2>
+        <p>
+          Similarly to llamify, this monkey patch affects all words in your app, but instead
+          scrambles all the letters in every word on the page (excluding the first & last letter).
+        </p>
+        <p>
+          Again, for demonstration purposes there are a bunch of varying elements below that are translated.
+        </p>
+        <BasicElements />
       </div>
     );
   }
